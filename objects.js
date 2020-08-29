6a5f184136585200017f0cd2b0be8459e2382a91
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-29 00:56:07 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -14,7 +14,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   
-  delete artistName.playlist
+  delete(artistName)
   
   return(playlist);
 }
