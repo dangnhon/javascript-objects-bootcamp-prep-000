16f804d568bff1b734ef3f5beaf258935eb59922
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-29 00:25:35 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playslist = {
+  rock: "jrock",
+  anime: "miku",
+  dubstep: "drop"
+}
+
+function updatePlaylist(playlist, artist name, song title) {
+  
+  playlist['artist name', 'song title']
+}
+
+
+
